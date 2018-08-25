9748740e7774c746540662af1235252b6470b556
MoneroOcean <support@moneroocean.stream>
2018-08-25 22:22:09 +0200

    Inlined streaming-worker implementation

--- sickle.js
+++ sickle.js
@@ -1,10 +1,40 @@
 "use strict";
 
 const path   = require("path");
-const worker = require("streaming-worker");
+const events = require('events');
 
-const addon_path  = path.join(__dirname, "node_modules/sickle_core/build/Release/sickle_core.node");
-const sickle_core = worker(addon_path, { foo: "bar" });
+function core(cpp_entry_point, opts) {
+    const stream2node = require(cpp_entry_point);
+    var emitter = new events();
+
+    var worker = new stream2node.StreamingWorker(
+        function(event, value){
+            emitter.emit(event, value);
+        }, 
+        function () {
+            emitter.emit("close");
+        }, 
+        function(error) {
+            emitter.emit("error", error);
+        }, 
+        opts
+    );
+
+    var sw = {};
+    
+    sw.from = emitter;
+    sw.to = {
+        emit: function(name, data) {
+            worker.sendToAddon(name, data);
+        }
+    }
+    sw.close = worker.closeInput;
+               
+    return sw;
+}
+
+const core_path   = path.join(__dirname, "node_modules/sickle_core/build/Release/sickle_core.node");
+const sickle_core = code(core_path, { foo: "bar" });
 
 sickle_core.from.on('integer', function(value) {
   console.log(value);

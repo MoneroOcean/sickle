cf42ba47ca6811e11a0a3263c212f8f488a4979e
MoneroOcean <support@moneroocean.stream>
2018-08-25 21:10:55 +0200

    Experimental worker implementation

--- sickle.js
+++ sickle.js
@@ -1,11 +1,16 @@
 "use strict";
 
-var multiHashing = require('cryptonight-hashing');
+const multiHashing = require('cryptonight-hashing');
 
-var data = new Buffer("7000000001e980924e4e1109230383e66d62945ff8e749903bea4336755c00000000000051928aff1b4d72416173a8c3948159a09a73ac3bb556aa6bfbcad1a85da7f4c1d13350531e24031b939b9e2b", "hex");
+const data = new Buffer("7000000001e980924e4e1109230383e66d62945ff8e749903bea4336755c00000000000051928aff1b4d72416173a8c3948159a09a73ac3bb556aa6bfbcad1a85da7f4c1d13350531e24031b939b9e2b", "hex");
 
-multiHashing.cryptonight_async_worker(data, 1, function(str) {
+let worker = multiHashing.cryptonight_async_worker(data, 1, function(str) {
   console.log("RUN: " + str);
 }, function() {
   console.log("END");
 });
+
+worker.update_worker("dddd", "eeeeeeee");
+
+let num = 0;
+setInterval(1000, function() { worker.update_worker("dddd" + ++num, "eeeeeeee"); });

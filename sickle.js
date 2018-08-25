61197890b32b1b7527ac714595e2d0e07420b734
MoneroOcean <support@moneroocean.stream>
2018-08-25 14:05:01 +0200

    Code rearrangement

--- sickle.js
+++ sickle.js
@@ -1,11 +1,11 @@
 "use strict";
 
+const path   = require("path");
 const worker = require("streaming-worker");
 
-const path = require("path");
-const addon_path = path.join(__dirname, "node_modules/sickle_core/build/Release/sickle_core.node");
-const sickle_core = worker(addon_path, {foo: "bar"});
+const addon_path  = path.join(__dirname, "node_modules/sickle_core/build/Release/sickle_core.node");
+const sickle_core = worker(addon_path, { foo: "bar" });
 
-sickle_core.from.on('integer', function(value){
-    console.log(value);
+sickle_core.from.on('integer', function(value) {
+  console.log(value);
 });

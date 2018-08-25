7206f5c9bab7c9df6bdc0a84f407fb77824fc3bc
MoneroOcean <support@moneroocean.stream>
2018-08-25 18:16:46 +0200

    Switched to sickle branch of node-cryptonight-hashing

--- sickle.js
+++ sickle.js
@@ -1,11 +1,7 @@
 "use strict";
 
-const path   = require("path");
-const worker = require("streaming-worker");
+var multiHashing = require('cryptonight-hashing');
 
-const addon_path  = path.join(__dirname, "node_modules/sickle_core/build/Release/sickle_core.node");
-const sickle_core = worker(addon_path, { foo: "bar" });
+var data = new Buffer("7000000001e980924e4e1109230383e66d62945ff8e749903bea4336755c00000000000051928aff1b4d72416173a8c3948159a09a73ac3bb556aa6bfbcad1a85da7f4c1d13350531e24031b939b9e2b", "hex");
 
-sickle_core.from.on('integer', function(value) {
-  console.log(value);
-});
+console.log(multiHashing.cryptonight(data));

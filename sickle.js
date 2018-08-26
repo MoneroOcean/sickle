8b752bfa87ed3e8da9fdc16ddf2f079bea1e65e9
MoneroOcean <support@moneroocean.stream>
2018-08-26 10:51:34 +0200

    Code fixes

--- sickle.js
+++ sickle.js
@@ -3,13 +3,11 @@
 const path   = require("path");
 const events = require("events");
 
-var worker;
-
 function core(core_name, opts) {
     const core_path   = path.join(__dirname, "node_modules/" + core_name + "/build/Release/" + core_name + ".node");
     const core_module = require(core_path);
     var emitter = new events();
-    worker = new core_module.AsyncWorker(
+    var worker = new core_module.AsyncWorker(
         function(event, value) { emitter.emit(event, value); },
         function () { emitter.emit("close"); },
         function(error) { emitter.emit("error", error); },
@@ -29,13 +27,10 @@ sickle_core.from.on('integer', function(value) {
 
 let i = 0;
 setInterval(function() {
-    console.log("x4");
     if (sickle_core) sickle_core.emit_to("input" + ++i, "here's something");
 }, 100);
 
 setTimeout(function() {
-    const sickle_core2 = sickle_core;
+    sickle_core.emit_to("close", "xxx");
     sickle_core = null;
-    sickle_core2.emit_to("close", "xxx");
-    worker = null;
 }, 5*1000);
